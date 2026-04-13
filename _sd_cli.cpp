d61768baaa40bbced80c605adf61f03eb85c6723
Roj234 <82699138+roj234@users.noreply.github.com>
2026-04-14 02:55:40 +0800

    更新sdcpp

--- _sd_cli.cpp
+++ _sd_cli.cpp
@@ -1,5 +1,6 @@
 
 #include "cli/main.cpp"
 #include "cli/image_metadata.cpp"
+#include "common/common.cpp"
 #include "common/log.cpp"
 #include "common/media_io.cpp"
--- _sd_server.cpp
+++ _sd_server.cpp
@@ -1,4 +1,5 @@
 
 #include "server/main.cpp"
+#include "common/common.cpp"
 #include "common/log.cpp"
 #include "common/media_io.cpp"
